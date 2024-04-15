bf829989ffc5ccabc1c245b39a783b7565386812
Chalkin Vasily <calkin9797@gmaul.com>
2024-04-15 21:35:23 +0500

    add docstring to str_func methods

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def str_upper(some_str):
+    """
+    Return a string with all uppercase
+    """
     return some_str.upper()
+
+
+def str_title(some_str):
+    """
+    Return 'some_str' in title case.
+    """
+    return some_str.title()
